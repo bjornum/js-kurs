74d88aa2f213efa85339ba39ce5b44a6d3cdf810
bjornum <37101863+bjornum@users.noreply.github.com>
2018-03-21 14:20:09 +0100

    learning pages

---
+++ js/app.js
@@ -0,0 +1,31 @@
+
+var customHeader = "something";
+var heading = document.querySelector(`h1`);
+heading.textcontent = customHeader;
+
+
+var number1= 2;
+var number2= 3;
+var texts ="fancy";
+var mylist = ["one", "two", "three"]
+var myobject = {}
+
+function multiplyByTwo(n1, n2){
+    var val = n2 + n1;
+return val * 2;
+}
+
+function addText(){
+   var el = document.getElementById("textChange");
+   
+   if (el.innerHTML ===  "rofl"){
+   el.innerText = null;
+} else {
+    el.innerText = "rofl"
+}
+
+
+function addText2(){
+    document.getElementById("textChange").innerHTML = "knapp nummer 2"
+}
+
