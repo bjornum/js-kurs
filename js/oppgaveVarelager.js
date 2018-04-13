150c09c241f7f4be612a8ec48667bb6948cb8f5b
bjornum <37101863+bjornum@users.noreply.github.com>
2018-04-13 13:48:40 +0200

    .

---
+++ js/oppgaveVarelager.js
@@ -0,0 +1,30 @@
+var vareliste = [{
+    id:1,
+    varenavn:"mus",
+    pris:500,
+    antall:15
+},{
+    id:2,
+    varenavn:"tastatur",
+    pris:899,
+    antall:0
+},{
+    id:3,
+    varenavn:"skjerm",
+    pris:2999,
+    antall:3
+},
+
+];
+
+var vareobjecter =[];
+    var i;
+    for(var i=0; i<vareliste.length; i++){
+        var vareobject = vareliste[i];
+        //vareobject.id
+        console.log(vareobject)
+    }
+
+
+
+    console.log(vareobject,vareobjecter, )
